f01e4b1feff0710b9187ea555c088ef4266c099f
PixelGuy101 <kulikov-me@yandex.ru>
2017-11-19 22:33:49 +0300

    Add files via upload

--- Return_To_Innocence/MainGameFile.py
+++ Return_To_Innocence/MainGameFile.py
@@ -2,6 +2,7 @@ import kivy
 kivy.require('1.10.0')
 from kivy.lang import Builder
 from kivy.app import App
+from kivy.core.audio import SoundLoader
 from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
 
 Builder.load_file('main.kv')
@@ -11,18 +12,66 @@ class MenuScreen(Screen):
 class GalleryScreen(Screen):
     pass
 class AboutScreen(Screen):
-    def createtext(self, index):
-        arr = ['Первый пробный текст','Второй пробный текст','Третий пробный текст']
-        return arr[index]
-
+    pass
+class PlayScreen(Screen):
+    pass
 sm = ScreenManager(transition=WipeTransition())
 sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(PlayScreen(name='play'))
 sm.add_widget(GalleryScreen(name='gallery'))
 sm.add_widget(AboutScreen(name='about'))
 
+#--------------MainClass---------------
+
 class MainApp(App):
+
     def build(self):
+        self.load_sound()
         return sm
+
+    def load_sound(self):
+        self.sound = SoundLoader.load('Music\IGotLove.mp3')
+        self.sound.play()
+        self.mute_index = True
+
+    def stop_play_sound(self):
+        if self.sound is not None and self.mute_index:
+            self.sound.stop()
+            self.mute_index = False
+        else:
+            self.sound.play()
+            self.mute_index = True
+
+
+    def create_text(self, chapter):
+        self.pos = 0
+        input = open('Story\Chapter_'+str(chapter), 'r', encoding='UTF-8')
+        self.A=[]
+        while True:
+            tmp = input.readline().rsplit()
+            if tmp == []:
+                break
+            else:
+                self.A.append(tmp)
+
+        for i in range(len(self.A)):
+            self.A[i]=" ".join(list(map(str,self.A[i])))
+
+
+
+
+
+    def text_show(self,x):
+        #TODO тута надо будет доделать чтобы после прочтения вызывался следующий экран и чтобы это работало для каждой главы
+
+        if self.pos + x == -1 or self.pos + x == 3:
+            pass
+        else:
+            self.pos += x
+
+        if self.pos > -1 and self.pos < 3:
+            return str(self.A[self.pos])
+
 
 
 if __name__ == '__main__':
